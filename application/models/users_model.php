d492ae654ae19a089e34d4d17d0f391a97fa9d78
Sapayth-Lab <sapayth@users.noreply.github.com>
2019-08-20 18:49:37 +0600

    initial commit

---
+++ application/models/users_model.php
@@ -0,0 +1,12 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_model extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+
+	public function get_all_users() {
+		return $this->db->get('users');
+	}
+}
